986df49e8193d61bc40f3e6318fe282e09cdb73b
Chen <chen7r7@gmail.com>
2022-05-29 21:01:34 +0300

    add start script

--- app.js
+++ app.js
@@ -226,6 +226,7 @@ app.use((err,req, res, next) =>{
     res.status(statusCode).render('error', {err});
 })
 
-app.listen('3000',() => {
-    console.log('Serving on Port 3000');
+const port = process.env.PORT || 3000 //3000 needs to be in ""?
+app.listen(port, () => {
+    console.log(`Serving on Port ${port}`);
 });
